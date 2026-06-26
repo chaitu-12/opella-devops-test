b19836a5cb2615041705e3dea9817a7911c86cb2
chaitu-12 <chaitanyakanchiraju@gmail.com>
2026-06-26 19:12:52 +0530

    tf plan

---
+++ modules/vm/main.tf
@@ -0,0 +1,52 @@
+resource "azurerm_linux_virtual_machine" "this" {
+
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  size = var.vm_size
+
+  admin_username = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.this.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+
+  }
+
+  os_disk {
+
+    caching              = "ReadWrite"
+
+    storage_account_type = "Standard_LRS"
+
+  }
+
+  source_image_reference {
+
+    publisher = "Canonical"
+
+    offer = "ubuntu-24_04-lts"
+
+    sku = "server"
+
+    version = "latest"
+
+  }
+
+  identity {
+
+    type = "SystemAssigned"
+
+  }
+
+  tags = local.tags
+
+}
